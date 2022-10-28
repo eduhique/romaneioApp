b05cba4ec5b9d1b58e147cffbff4d9535240111d
Eduardo Pontes <eduardohenrique38@live.com>
2022-10-28 00:52:12 -0300

    Iniciando Projeto

---
+++ src/test/java/com/eduardopontes/romaneioapp/RomaneioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardopontes.romaneioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaneioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
